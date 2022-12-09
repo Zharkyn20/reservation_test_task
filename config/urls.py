f82ced4f1732b20dd1e19d8d822050439e6abcb1
zharkynaskat <61348606+zharkynaskat@users.noreply.github.com>
2022-12-09 16:56:17 +0600

    Add reservation view. In Home Page shows table of reservations, ordered by rental id. Links previous reservation and shows it in "previous reservation, ID" column

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""config URL Configuration
+"""euro_testt URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rent.views import reservation_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', reservation_view)
 ]
--- rent/views.py
+++ rent/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rent.models import Reservation, Rental
+from rent.utils import link_previous
+
+
+def reservation_view(request):
+    rentals = Rental.objects.all()
+    all_reservations = Reservation.objects.none()
+    prev_reservations = {}
+
+    for rental in rentals:
+        reservations = Reservation.objects.filter(rental=rental)
+        all_reservations |= reservations
+        prev_reservations |= link_previous(reservations)
+
+    return render(
+        request,
+        'index.html',
+        context={"reservations": all_reservations, 'prev_reservations': prev_reservations})
+
+
