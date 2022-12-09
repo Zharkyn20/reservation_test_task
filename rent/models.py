e21922ddaa28fd30c790ab538b597c4e71003f66
zharkynaskat <61348606+zharkynaskat@users.noreply.github.com>
2022-12-09 16:46:53 +0600

    Add Entities: Rent, Reservation

--- rent/models.py
+++ rent/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Rental(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    rental = models.ForeignKey(
+        "Rental", on_delete=models.PROTECT, related_name="reservations"
+    )
+    checkin = models.DateField()
+    checkout = models.DateField()
+
+    def __str__(self):
+        return f"Res-{self.id} {self.id}"
